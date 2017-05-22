b1295b52d85e538d0cec3af498c8798810566bbf
studentAA <aa.student@o2.pl>
2017-05-22 20:03:16 +0200

    bugfix

--- test/app.spec.js
+++ test/app.spec.js
@@ -4,7 +4,7 @@ var expect = require('chai').expect;
 var app = require('../app/app');
 
 describe('getDescendingNumbers', function (){
-    it.only('should return string with numbers separated by spaces', function ()    {
+    it('should return string with numbers separated by spaces', function ()    {
         expect(app.getDescendingNumbers(15, 1)).to.eql('15 14 13 12 11 10 9 8 7 6 5 4 3 2 1');
     });
     it('should return false if start greater than stop', function ()    {
@@ -14,11 +14,7 @@ describe('getDescendingNumbers', function (){
     it('should return false if start equal stop', function ()    {
         expect(app.getDescendingNumbers(13, 13)).to.eql(false);
     });
-    it.skip('should return false if start is string', function ()    {
+    it('should return false if start is string', function ()    {
         expect(app.getDescendingNumbers('asa', 1)).to.eql(false);
-    });
-    it('should ...', function ()
-    {
-
     });
 });
