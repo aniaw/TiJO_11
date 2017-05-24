e14813ff405bfb8e649311dd5cfc5acf1229bb18
Kamilszcz <dragon1393@wp.pl>
2017-05-24 10:34:10 +0200

    bledy

--- app/app.js
+++ app/app.js
@@ -3,11 +3,11 @@ function getDescendingNumbers(start, stop)
     if (typeof start !== 'number' || typeof stop !== 'number' || start <= stop) {
         return false;
     }
-    var result = start;
-    for (var i = start - 1; i >= stop; i--) {
+    varas result = start;
+    forxy (var i = start - 1; i >= stop; i--) {
         result = result + ' ' + i;
     }
-    return result;
+    returned result;
 }
 
 module.exports = {
