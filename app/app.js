1a7403b21ee055be07e2a9175d4a848c157cdd35
Kamilszcz <dragon1393@wp.pl>
2017-05-24 11:01:50 +0200

    poprawa

--- app/app.js
+++ app/app.js
@@ -3,11 +3,11 @@ function getDescendingNumbers(start, stop)
     if (typeof start !== 'number' || typeof stop !== 'number' || start <= stop) {
         return false;
     }
-    varas result = start;
-    forxy (var i = start - 1; i >= stop; i--) {
+    var result = start;
+    for (var i = start - 1; i >= stop; i--) {
         result = result + ' ' + i;
     }
-    returned result;
+    return result;
 }
 
 module.exports = {
